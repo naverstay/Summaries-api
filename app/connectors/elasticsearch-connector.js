2f0255a009d62982d98399699354919027e398a6
vitaliy <khotey.v@ya.ru>
2017-11-21 21:47:37 +0200

    removed elasticsearch

--- app/connectors/elasticsearch-connector.js
+++
@@ -1,8 +0,0 @@
-import elasticsearch from 'elasticsearch';
-import { ELASTIC_SEARCH_URL } from '../config';
-
-const client = new elasticsearch.Client({
-  host: ELASTIC_SEARCH_URL,
-});
-
-export default client;
